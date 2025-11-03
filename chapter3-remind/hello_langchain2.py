d0b243e36d6ce8c3310adb3eda44bf4916092498
suku-7 <ryumon6550@gmail.com>
2025-11-03 16:36:44 +0900

    Langchain복습 - 클로드 호출

---
+++ chapter3-remind/hello_langchain2.py
@@ -0,0 +1,6 @@
+from langchain.chat_models import init_chat_model
+
+model = init_chat_model("claude-sonnet-4-20250514", model_provider="anthropic")
+result = model.invoke("랭체인이 뭔가요?")
+
+print(result.content)
